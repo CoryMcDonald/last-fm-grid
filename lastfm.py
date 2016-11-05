ab6603cd34185d21e2c6d9e489945389df0db795
Cory McDonald <cory.mcdonald@cerner.com>
2016-11-05 11:10:25 -0500

    Add grid work

--- lastfm.py
+++ lastfm.py
@@ -11,7 +11,7 @@ def main():
     file_names = []
 
     for album in albums:
-        album_image = album.item.get_cover_image(pylast.COVER_MEDIUM)
+        album_image = album.item.get_cover_image(pylast.COVER_EXTRA_LARGE)
         file_name = album_image[album_image.rfind('/')+1:]
         if not os.path.isfile(file_name):
             print 'Downloading', file_name
@@ -19,6 +19,16 @@ def main():
             image_file.retrieve(album_image, file_name)
         file_names.append(file_name)
     
-        
+    grid_image = Image.new('RGB', (900,900))
+
+    array_position = 0
+    for i in xrange(0,900,300):
+        for j in xrange(0,900,300):
+            im = Image.open(file_names[array_position])
+            grid_image.paste(im, (i,j))
+            array_position = array_position + 1
+    grid_image.save('ayy.jpg')    
+    grid_image.show()
 
 main()
+
